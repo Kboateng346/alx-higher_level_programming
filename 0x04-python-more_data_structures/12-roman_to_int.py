23adf3ea295db42f2d1f7cb37f5f979e7416c361
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-08-18 01:18:26 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_numm):
+    to_subb = 0
+    max_listt = max(list_numm)
+
+    for nn in list_numm:
+        if max_listt > nn:
+            to_subb += nn
+
+    return (max_listt - to_subb)
+
+
 def roman_to_int(roman_string):
-    # checks
     if not roman_string:
         return 0
+
     if not isinstance(roman_string, str):
         return 0
-    if not roman_string.isupper():
-        return 0
 
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+    rom_nn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keyss = list(rom_nn.keys())
 
-    result = 0
-    temp = list(roman_string)
-    # Concatenate 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and adds
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+    numm = 0
+    last_romm = 0
+    list_numm = [0]
+
+    for chh in roman_string:
+        for r_numm in list_keyss:
+            if r_numm == chh:
+                if rom_nn.get(chh) <= last_romm:
+                    numm += to_subtract(list_numm)
+                    list_numm = [rom_nn.get(chh)]
+                else:
+                    list_numm.append(rom_nn.get(chh))
+
+                last_romm = rom_nn.get(chh)
+
+    numm += to_subtract(list_numm)
+
+    return (numm)
