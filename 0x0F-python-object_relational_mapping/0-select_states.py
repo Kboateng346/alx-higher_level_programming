fbc87f43f0bec677be8ea39c16b18d89e1c724f8
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-11-19 01:41:42 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-This Script that lists all states from the database hbtn_0e_0_usa.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
-import sys
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    username, passw, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    db = MySQLdb.connect(user=username, passwd=passw, db=db_name)
-    cur = db.cursor()
+    db_cursor = db_connect.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
+    db_cursor.execute("SELECT * FROM states")
 
-    for row in rows:
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
